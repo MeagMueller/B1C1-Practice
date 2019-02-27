96dafa5b53b67edb778488bd864269fb120c787f
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-02-27 14:19:25 -0600

    Number two completed

--- src/scripts/blog.js
+++ src/scripts/blog.js
@@ -1,3 +1,11 @@
-const articleHeader= document.querySelector(".article__header");
+// const articleHeader= document.querySelector(".article__header");
 
-articleHeader.textContent="Welcome to the Meag Blog!";
+// articleHeader.textContent="Welcome to the Meag Blog!";
+
+const articleHeader = document.querySelectorAll(".article__header");
+
+for (let i=0; i<articleHeader.length; i++){
+    articleHeader[i].classList.add("important");
+}
+
+console.log(articleHeader); 
