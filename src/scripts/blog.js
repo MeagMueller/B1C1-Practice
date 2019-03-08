ba777905db2b89d72e5e4a25f51550aa695ec5e2
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-03-08 09:36:45 -0600

    Completed

--- src/scripts/blog.js
+++ src/scripts/blog.js
@@ -2,6 +2,7 @@ const articleHeaderChange= document.querySelector(".article__header");
 
 articleHeaderChange.textContent="Welcome to the Meag Blog!";
 
+// Use JavaScript to obtain a reference to all article__header elements and change their classList property value to article__header important.
 const articleHeaderClass = document.querySelectorAll(".article__header");
 
 for (let i=0; i<articleHeaderClass.length; i++){
@@ -9,3 +10,12 @@ for (let i=0; i<articleHeaderClass.length; i++){
 }
 
 console.log(articleHeaderClass, articleHeaderChange); 
+
+// Obtain a reference the element with a class of dashed and remove it.
+const dashed= document.querySelector(".dashed");
+dashed.classList.remove("dashed")
+
+// Obtain a reference the element with a class of article_footer and add the class of goldenrod it.
+const goldenrod= document.querySelector(".article__footer");
+goldenrod.classList.add("goldenrod");
+
